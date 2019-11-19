4ccbb2562bd6db2f880b482fcff0b0bc64b0d9aa
Francis Enemuo <38217960+francenemuo@users.noreply.github.com>
2019-11-19 17:46:29 +0100

    added first input tags

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'emb-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
